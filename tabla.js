dfeaa7a4c2a1b0b042d81224e2dbcae34c9381f5
Andújar <jlbandujar@gmail.com>
2021-11-13 09:13:25 +0100

    Segundo commit

--- tabla.js
+++ tabla.js
@@ -1,4 +1,7 @@
 const base=2;
+console.log("===========================");
+console.log("    tabla del ", base );
+console.log("===========================");
 for ( let i=0; i<=10;i++){
     console.log(`${i} * ${base}=${i*base}`)
 }
